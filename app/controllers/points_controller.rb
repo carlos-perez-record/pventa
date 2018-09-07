66a2667fb1e4f955a3659986dfa41bc11a4f046f
carlos-perez-record <carlos.perez@record.com.co>
2018-09-07 08:47:35 -0500

    Se crea el metodo create en el controlador, se hacen modificaciones en el fomrulario de registro, y se comienza a implementar dejandolo solo como comentario el campo para subir la foto de los puntos de venta

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -7,4 +7,18 @@ class PointsController < ApplicationController
     @p = Point.new
   end
 
+  def create
+    @p = Point.new(point_params)
+    if @p.save
+      redirect_to points_path, notice: "El Punto de Venta fue publicado con éxito"
+    else
+      render :new
+    end
+  end
+
+private
+  def point_params
+    params.require(:point).permit(:codigo, :nombre, :direccion, :telefono)
+  end
+
 end
