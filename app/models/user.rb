82a697c08db490e76d67c6840ddec4ee4bc32e1e
carlos-perez-record <carlos.perez@record.com.co>
2018-09-19 11:42:37 -0500

    Agrega validaciones al formulario de puntos de venta y al de registro de usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,10 @@
 class User < ApplicationRecord
   # Cifrado de la contraseña del usuario
   has_secure_password validations: false
+
+  validates :email, uniqueness: true, format: /@record.com.co/
+  validates :password, presence: true, on: :create
+  validates :password, length: { in: 6..20 }, allow_nil: true
+  validates :nombre, presence: true
+  validates :cedula, presence: true
 end
