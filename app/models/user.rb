f1500a4063bc58c5965c9cfd56a4a21c2bd81db8
carlos-perez-record <carlos.perez@record.com.co>
2018-09-19 10:40:49 -0500

    Se instala annotate y se corre, para anotar los modelos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string(255)
+#  nombre          :string(255)
+#  cedula          :integer
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   # Cifrado de la contraseña del usuario
   has_secure_password validations: false
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string(255)
+#  nombre          :string(255)
+#  cedula          :integer
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
