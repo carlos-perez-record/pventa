ee643275a6275f6af69e704adc1d28403396816b
carlos-perez-record <carlos.perez@record.com.co>
2018-10-22 18:03:24 -0500

    Se reversa una migracion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_143134) do
+ActiveRecord::Schema.define(version: 2018_10_12_142614) do
 
   create_table "actives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ip"
@@ -18,8 +18,6 @@ ActiveRecord::Schema.define(version: 2018_10_12_143134) do
     t.string "placa"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "devices_id"
-    t.index ["devices_id"], name: "index_actives_on_devices_id"
   end
 
   create_table "centers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -74,7 +72,6 @@ ActiveRecord::Schema.define(version: 2018_10_12_143134) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "actives", "devices", column: "devices_id"
   add_foreign_key "centers", "zones"
   add_foreign_key "points", "centers"
   add_foreign_key "points", "technologies"
