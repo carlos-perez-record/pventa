e8d23ddf3f0e586a11efc6bf7af0fc447acbd8bb
carlos-perez-record <carlos.perez@record.com.co>
2018-10-22 18:07:58 -0500

    Se elimina la tabla actives

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_142614) do
-
-  create_table "actives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "ip"
-    t.string "serial"
-    t.string "placa"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_10_11_212027) do
 
   create_table "centers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nombre"
