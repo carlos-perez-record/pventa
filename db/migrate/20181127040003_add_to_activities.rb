6b4bae6484223495323f875a2c25282b5202622d
carlos-perez-record <carlos.perez@record.com.co>
2018-11-26 23:01:04 -0500

    Se agrega una llave foranea de reformas a las actividades

---
+++ db/migrate/20181127040003_add_to_activities.rb
@@ -0,0 +1,4 @@
+class AddToActivities < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_212602) do
+ActiveRecord::Schema.define(version: 2018_11_27_040003) do
 
   create_table "actives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ip"
