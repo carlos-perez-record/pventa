0740768cc3929447e8ec4a9b250ae71e60a4553c
carlos-perez-record <carlos.perez@record.com.co>
2020-04-20 15:56:59 -0500

    Se hace migracion para replicar la nueva tabla uses en la base de datos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_134515) do
+ActiveRecord::Schema.define(version: 2020_04_20_205329) do
 
   create_table "actives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "serial"
@@ -108,6 +108,12 @@ ActiveRecord::Schema.define(version: 2020_04_20_134515) do
     t.string "nombre"
     t.integer "cedula"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "uses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
