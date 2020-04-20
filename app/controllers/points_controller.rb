367cd75a6b89a26a79286ba00fbcbe768a152789
carlos-perez-record <carlos.perez@record.com.co>
2020-04-19 21:55:58 -0500

    Se elimina celda y los vestigios de serial y celda del controlador points

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -55,7 +55,7 @@ class PointsController < ApplicationController
 
 private
   def point_params
-    params.require(:point).permit(:codigo, :nombre, :direccion, :telefono, :picture, :technology_id, :serial, :celda, :center_id)
+    params.require(:point).permit(:codigo, :nombre, :direccion, :telefono, :picture, :center_id)
   end
 
 
