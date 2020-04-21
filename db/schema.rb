0d862fb406c34c805a3b0ba9075ff205a459d88b
carlos-perez-record <carlos.perez@record.com.co>
2020-04-21 10:24:01 -0500

    Se hace la migracion de la nueva tabla ip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_205329) do
+ActiveRecord::Schema.define(version: 2020_04_21_145559) do
 
   create_table "actives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "serial"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 2020_04_20_205329) do
   create_table "devices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nombre"
     t.string "tipo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ips", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
